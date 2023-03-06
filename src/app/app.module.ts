aa9790871946f0a2b77622d7224b46626798183e
Zachary Svoboda <cczac1@gmail.com>
2023-03-05 21:53:50 -0500

    Setup material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AbilitiesComponent } from './ability-score/ability-score.component';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AbilitiesComponent
+    AbilitiesComponent,
+    BrowserAnimationsModule
   ],
   bootstrap: [AppComponent]
 })
