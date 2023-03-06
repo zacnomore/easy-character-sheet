35bfc956fc16f234ec23c1b12c1fadc5703c6a38
Zachary Svoboda <cczac1@gmail.com>
2023-03-05 23:05:09 -0500

    Setup tests

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HeaderComponent } from './header.component';
 
@@ -8,7 +9,7 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent],
+      imports: [HeaderComponent, NoopAnimationsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
