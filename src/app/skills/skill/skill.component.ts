7b04fc9d480a25f1d1f712d8b57fdfe7a0665fe4
Zachary Svoboda <cczac1@gmail.com>
2023-03-06 08:17:09 -0500

    Skills as fields

--- src/app/skills/skill/skill.component.ts
+++ src/app/skills/skill/skill.component.ts
@@ -1,11 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'ecs-skill',
   standalone: true,
-  imports: [CommonModule, MatCheckboxModule],
+  imports: [
+    CommonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './skill.component.html',
   styleUrls: ['./skill.component.scss'],
 })
