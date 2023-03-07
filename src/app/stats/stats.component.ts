a2c88671e9eb7e6d6fec5a9e461968ec0bfff922
Zachary Svoboda <cczac1@gmail.com>
2023-03-06 20:35:46 -0500

    Move sections under page

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AbilitiesComponent } from '../abilities/abilities.component';
-import { HeaderComponent } from '../header/header.component';
-import { SavingThrowsComponent } from '../saving-throws/saving-throws.component';
-import { SkillsComponent } from '../skills/skills.component';
-import { StatusComponent } from '../status/status.component';
-import { AttacksComponent } from '../attacks/attacks.component';
-import { EquipmentComponent } from '../equipment/equipment.component';
-import { BackgroundComponent } from '../background/background.component';
+import { AbilitiesComponent } from './abilities/abilities.component';
+import { HeaderComponent } from './header/header.component';
+import { SavingThrowsComponent } from './saving-throws/saving-throws.component';
+import { SkillsComponent } from './skills/skills.component';
+import { StatusComponent } from './status/status.component';
+import { AttacksComponent } from './attacks/attacks.component';
+import { EquipmentComponent } from './equipment/equipment.component';
+import { BackgroundComponent } from './background/background.component';
 
 @Component({
   selector: 'ecs-stats',
